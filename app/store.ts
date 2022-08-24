3eaa009b5dc40a00f97a4b22a9f65b459d8ed6fa
Luis Urrutia <luis@urrutia.me>
2022-08-23 22:47:06 -0400

    chore: add prettier, remove stories and fix redux

--- app/store.ts
+++ app/store.ts
@@ -1,19 +1,20 @@
-import {configureStore, ThunkAction} from '@reduxjs/toolkit';
-import {Action} from 'redux';
-import {createWrapper} from 'next-redux-wrapper';
-
-
+import { configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { Action } from "redux";
+import { createWrapper } from "next-redux-wrapper";
 
 const makeStore = () =>
-    configureStore({
-        reducer: {
-        },
-        devTools: true,
-    });
-
+  configureStore({
+    reducer: {},
+    devTools: true,
+  });
 
 export type AppStore = ReturnType<typeof makeStore>;
-export type AppState = ReturnType<AppStore['getState']>;
-export type AppDispatch = ReturnType<AppStore['dispatch']>;
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action>;
+export type AppState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = ReturnType<AppStore["dispatch"]>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  AppState,
+  unknown,
+  Action
+>;
 export const wrapper = createWrapper<AppStore>(makeStore);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import React, {FC} from 'react';
-import type { AppProps } from 'next/app'
-import {wrapper} from '../app/store';
+import "../styles/globals.css";
+import React, { FC } from "react";
+import type { AppProps } from "next/app";
+import { wrapper } from "../app/store";
 
-const WrappedApp: FC<AppProps> = ({Component, pageProps}) => <Component {...pageProps} />;
+const WrappedApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
 
 export default wrapper.withRedux(WrappedApp);
